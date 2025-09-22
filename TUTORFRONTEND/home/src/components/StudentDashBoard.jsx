8c62addabae389389062e1c61a815366627eb069
lasya06-tech <2300030060cseh@gmail.com>
2025-09-23 01:25:42 +0530

    updated code

--- TUTORFRONTEND/home/src/components/StudentDashBoard.jsx
+++ TUTORFRONTEND/home/src/components/StudentDashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./StudentDashboard.css";
+import "./StudentDashBoard.css";
 
 const StudentDashboard = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
