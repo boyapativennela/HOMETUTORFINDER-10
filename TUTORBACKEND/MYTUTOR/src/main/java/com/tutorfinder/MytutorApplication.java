5267eabaa84f3d49368d2b9bfd9585a0c8a7eebb
lasya06-tech <2300030060cseh@gmail.com>
2025-09-23 23:42:19 +0530

    updated code

--- TUTORBACKEND/MYTUTOR/src/main/java/com/tutorfinder/MytutorApplication.java
+++ TUTORBACKEND/MYTUTOR/src/main/java/com/tutorfinder/MytutorApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MytutorApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(MytutorApplication.class, args);
 		System.out.println("Application is running ");
+		System.out.flush();
 	}
 
 }
