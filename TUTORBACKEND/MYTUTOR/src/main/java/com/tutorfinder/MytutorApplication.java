5941011f527dde404db13fc1db725c45276a8e26
lasya06-tech <2300030060cseh@gmail.com>
2025-09-23 15:48:21 +0530

    updated code

--- TUTORBACKEND/MYTUTOR/src/main/java/com/tutorfinder/MytutorApplication.java
+++ TUTORBACKEND/MYTUTOR/src/main/java/com/tutorfinder/MytutorApplication.java
@@ -8,6 +8,7 @@ public class MytutorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MytutorApplication.class, args);
+		System.out.println("Application is running ");
 	}
 
 }
